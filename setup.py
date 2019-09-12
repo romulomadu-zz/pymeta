9079cae3c2bd3f9536f083c012070dabaa73717e
romulomadu <romulomadu@gmail.com>
2019-09-12 01:55:47 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pymeta",
+    version="0.1",
+    install_requires=['pandas', 'numpy', 'scikit-learn', 'statsmodels', 'networkx', 'scipy']
+)
